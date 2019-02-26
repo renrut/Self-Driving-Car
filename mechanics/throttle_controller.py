7e4af38fa493b5fe8c9902a8977997da0a49f87c
r3nrut <winston.t.strayhorn@vanderbilt.edu>
2019-02-26 00:10:26 -0800

    Testing pwm vals

--- mechanics/throttle_controller.py
+++ mechanics/throttle_controller.py
@@ -5,16 +5,21 @@ class ThrottleController:
     OFF = 0
     FULL = 1
     CHANNEL = 0
+    controller = None,
+    max_pulse = 300,
+    min_pulse = 490,
+    zero_pulse = 350
 
     def __init__(self, offvalue, fullvalue):
         self.offvalue = offvalue
         self.fullvalue = fullvalue
         self.servoController = ServoController(self.CHANNEL)
+        self.servoController.set_pwm(self.zero_pulse)
 
     # Takes a steer value between OFF and FULL and calculates it to the PWM
     def calculate_steering(self, throttle):
-        difference = self.fullvalue - self.offvalue
-        return int(self.offvalue + (difference * throttle))
+        difference = self.max_pulse - self.min_pulse
+        return int(self.min_pulse + (difference * throttle))
 
     def set_throttle(self, steervalue):
         pwmval = self.calculate_steering(steervalue)
