8acec2ef14eed0d6096e46e14bc21a9c2b191f6c
r3nrut <winston.t.strayhorn@vanderbilt.edu>
2019-03-09 13:15:23 -0800

    Restructuring web server.

--- mechanics/camera_controller.py
+++ mechanics/camera_controller.py
@@ -1,9 +1,7 @@
-import io
 import picamera
-import logging
-import socketserver
+import io
 from threading import Condition
-from http import server
+
 
 class StreamingOutput(object):
     def __init__(self):
@@ -22,63 +20,16 @@ class StreamingOutput(object):
             self.buffer.seek(0)
         return self.buffer.write(buf)
 
-class StreamingHandler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            f = open('../web/frontpage.html', 'rb')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.end_headers()
-            self.wfile.write(f.read())
-            f.close()
-        elif self.path == '/js/socketin.js':
-            f = open('../web/js/socketin.js', 'rb')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/javascript')
-            self.end_headers()
-            self.wfile.write(f.read())
-            f.close()
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
-
-class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
+class CameraController:
+    def __init__(self, resolution='640x480', framerate=24):
+        self.resolution = resolution
+        self.framerate = framerate
+        self.output = StreamingOutput()
+        self.camera = picamera.PiCamera(resolution=self.resolution, framerate=self.framerate)
 
 
-with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-    output = StreamingOutput()
-    camera.start_recording(output, format='mjpeg')
-    try:
-        address = ('', 8000)
-        server = StreamingServer(address, StreamingHandler)
-        server.serve_forever()
-    finally:
-        camera.stop_recording()
+    def start(self):
+        self.camera.start_recording(self.output, format='mjpeg')
+
+    def stop(self):
+        self.camera.stop_recording()
---
+++ web_server.py
@@ -0,0 +1,65 @@
+import logging
+import socketserver
+from http import server
+from mechanics.camera_controller import CameraController
+
+
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            f = open('../web/frontpage.html', 'rb')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
+        elif self.path == '/js/socketin.js':
+            f = open('../web/js/socketin.js', 'rb')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/javascript')
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+with CameraController() as camera:
+    camera.start()
+    try:
+        address = ('', 8000)
+        server = StreamingServer(address, StreamingHandler)
+        server.serve_forever()
+    finally:
+        camera.stop_recording()
