80963f7989eb14785dbc500c9841672003dd4836
r3nrut <winston.t.strayhorn@vanderbilt.edu>
2019-03-09 14:07:13 -0800

    Better steering from the client

--- web/js/socketin.js
+++ web/js/socketin.js
@@ -1,24 +1,49 @@
-
 var socket = new WebSocket("ws://192.168.86.41:8765");
 
+
 socket.onopen = function(){
-    console.log("Conn open...");
-    document.onkeypress = function (e) {
-        e = e || window.event;
-        var code = String.fromCharCode(e.keyCode);
-        if (code == 'w') {
-            socket.send("0.0,0.5")
-        }
-        else if (code == 'a') {
-            socket.send("0.75,0.0")
-        }
-        else if (code == 's') {
-            socket.send("0.0,-0.2")
-        }
-        else if (code == 'd') {
-            socket.send("-0.75,0.0")
-        }
-    };
+
+var set = new Set(); // You could also use an array
+
+console.log("Conn open...");
+
+	var sendInput = function()
+	{
+		var throttle = 0;
+		var steering = 0;
+		if(set.has('w'))
+		{
+			throttle += .5;
+		}
+		if(set.has('s'))
+		{
+			throttle -= .5;
+		}
+		if(set.has('a'))
+		{
+			steering += .5;
+		}
+		if(set.has('d'))
+		{
+			steering -= .5;
+		}
+		var send = steering.toString() + "," + throttle.toString();
+        console.log("Sending" + send);
+		socket.send(send);
+	}
+
+	onkeydown = onkeyup = function(e){
+	    e = e || event; // to deal with IE
+	    if(e.type == 'keydown')
+	    {
+	    	set.add(String.fromCharCode(e.keyCode).toLowerCase())
+	    }
+	    if(e.type == 'keyup')
+	    {
+			set.delete(String.fromCharCode(e.keyCode).toLowerCase())
+	    }
+	    sendInput();
+	}
 }
 
 socket.onclose = function (evt) {
